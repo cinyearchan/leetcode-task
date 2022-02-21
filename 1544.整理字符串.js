fd6d962fdd55d31584e10510cb39e56d88906c23
cinyearchan <cinyearchan@gmail.com>
2022-02-21 17:21:10 +0800

    feat: add 1544.整理字符串.js

---
+++ 1544.整理字符串.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=1544 lang=javascript
+ *
+ * [1544] 整理字符串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function (s) {
+  if (s.length === 0 || s.length === 1) {
+    return s;
+  }
+  let i = 0;
+  while (i <= s.length - 2 && s.length !== 0) {
+    if (
+      s[i] !== s[i + 1] &&
+      (s[i].toLowerCase() === s[i + 1] || s[i].toUpperCase() === s[i + 1])
+    ) {
+      s = s.slice(0, i) + s.slice(i + 2);
+      if (i > 0) {
+        i--;
+      }
+    } else {
+      i++;
+    }
+  }
+  return s;
+};
+// @lc code=end
